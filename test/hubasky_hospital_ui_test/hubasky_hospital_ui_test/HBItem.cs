1d31d8440ba229570f88145ca55a72d802e97cdb
Hazai Péter <hazaip@niif.hu>
2016-02-26 23:31:16 +0100

    Kezelőfelület VS project teszt + screenshotok

---
+++ test/hubasky_hospital_ui_test/hubasky_hospital_ui_test/HBItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hubasky_hospital_ui_test
+{
+    public class HBItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public string Quantity { get; set; }
+        public string Dimension { get; set; }
+
+        public HBItem(string id, string name, string dept, string qnt, string dim)
+        {
+            Id = id;
+            Name = name;
+            Dept = dept;
+            Quantity = qnt;
+            Dimension = dim;
+        }
+    }
+}
---
+++ test/hubasky_hospital_ui_test/hubasky_hospital_ui_test/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hubasky_hospital_ui_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<HBItem> hbitems = new List<HBItem>();
+            hbitems.Add(new HBItem("XA213424", "fecskendő", "mellkassebészet", "314", "db"));
+            hbitems.Add(new HBItem("BG123457", "gézlap", "mellkassebészet", "1230", "db"));
+            hbitems.Add(new HBItem("HB8234z1", "10-es tű", "mellkassebészet", "634", "db"));
+            hbitems.Add(new HBItem("ZHdq89389", "stetoszkóp", "intenzív osztály", "14", "db"));
+            hbitems.Add(new HBItem("HU123913", "gurulós hordágy", "sürgősségi osztály", "12", "db"));
+            hbitems.Add(new HBItem("JH893822", "szike", "szülészet", "184", "db"));
+            hbitems.Add(new HBItem("KAu738134", "orvosi köppeny", "kardiológia", "8", "db"));
+
+            dg_hb.ItemsSource = hbitems;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ProcedureWindow pr = new ProcedureWindow();
+            pr.Show();
+        }
+    }
+}
