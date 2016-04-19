45bcf3c8b646f98bb90f6d892792281934cf1ec3
Hazai Péter <hazaip@niif.hu>
2016-04-19 13:50:29 +0200

    plusz

--- sourcecode/HubaskyHospitalManager/HubaskyHospitalManager/Data/PopulateDb.cs
+++ sourcecode/HubaskyHospitalManager/HubaskyHospitalManager/Data/PopulateDb.cs
@@ -1,10 +1,12 @@
 ﻿using HubaskyHospitalManager.Model.ApplicationManagement;
 using HubaskyHospitalManager.Model.Common;
+using HubaskyHospitalManager.Model.HospitalManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HubaskyHospitalManager.Data
 {
@@ -12,6 +14,19 @@ namespace HubaskyHospitalManager.Data
     {
         public static void Populate(ApplicationManager appMgr)
         {
+            PopulateDb.PopulateUnits(appMgr);
+            //PopulateDb.PopulateEmployees(appMgr);
+
+            
+            
+        }
+
+        private static void PopulateEmployees(ApplicationManager appMgr)
+        {
+            var emps = appMgr.ApplicationDb.Employees.Select(m => m);
+
+            appMgr.ApplicationDb.Employees.RemoveRange(emps);
+
             Employee pista = new Employee("pista", "1234");
             pista.Name = "Istápoló István";
             pista.Phone = "+3630111";
@@ -33,7 +48,6 @@ namespace HubaskyHospitalManager.Data
             laci.Role = Role.Laboratorian;
             laci.DateOfBirth = "1986.01.06";
 
-
             Employee eszti = new Employee("eszti", "1234");
             eszti.Name = "Eszes Eszter";
             eszti.Phone = "+3630114";
@@ -53,6 +67,59 @@ namespace HubaskyHospitalManager.Data
             appMgr.ApplicationDb.Employees.Add(laci);
             appMgr.ApplicationDb.Employees.Add(eszti);
             appMgr.ApplicationDb.Employees.Add(andris);
+
+            appMgr.ApplicationDb.SaveChanges();
+        }
+
+        private static void PopulateUnits(ApplicationManager appMgr)
+        {
+            Employee hospMgr = null;
+            
+            var hosps = appMgr.ApplicationDb.Hospital.Select(m => m);
+            foreach (Hospital hosp in hosps)
+            {
+                hosp.Departments = new List<Department>();
+            }
+            var wards = appMgr.ApplicationDb.Wards.Select(w => w);
+            appMgr.ApplicationDb.Wards.RemoveRange(wards);
+            var depts = appMgr.ApplicationDb.Departments.Select(d => d);
+            appMgr.ApplicationDb.Departments.RemoveRange(depts);
+
+            appMgr.ApplicationDb.Hospital.RemoveRange(hosps);
+
+            var emps = appMgr.ApplicationDb.Employees.Select(m => m).ToList();
+            Hospital hospital = new Hospital();
+            hospital.Address = "1234 Budapest, Gyógyító köz 1.";
+            foreach (Employee emp in emps)
+            {
+                if ("eszti".Equals(emp.Username))
+                {
+                    hospMgr = emp;
+                }
+            }
+            hospital.Manager = hospMgr;
+            hospital.Name = "Hubasky Magánkórház";
+            hospital.Employees = emps;
+
+            Department patientTreatDept = new Department();
+            patientTreatDept.Name = "Betegellátási főosztály";
+            hospital.Departments.Add(patientTreatDept);
+            appMgr.ApplicationDb.Departments.Add(patientTreatDept);
+
+            Ward walkInPatientWard = new Ward();
+            walkInPatientWard.Name = "Járóbetegellátás";
+            walkInPatientWard.Manager = emps[0];
+            walkInPatientWard.Employees = emps;
+            appMgr.ApplicationDb.Wards.Add(walkInPatientWard);
+
+            Ward cardiologyWard = new Ward();
+            cardiologyWard.Name = "Kardiológia";
+            cardiologyWard.Manager = emps[1];
+            appMgr.ApplicationDb.Wards.Add(cardiologyWard);
+
+            patientTreatDept.Wards.Add(walkInPatientWard);
+            patientTreatDept.Wards.Add(cardiologyWard);
+            appMgr.ApplicationDb.Hospital.Add(hospital);
 
             appMgr.ApplicationDb.SaveChanges();
         }
