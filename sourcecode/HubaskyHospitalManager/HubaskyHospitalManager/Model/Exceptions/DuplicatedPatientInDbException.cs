7b337912ccac8be6878e99d7e89dd322df4e8200
Hazai Péter <hazaip@niif.hu>
2016-05-03 11:28:48 +0200

    exception

---
+++ sourcecode/HubaskyHospitalManager/HubaskyHospitalManager/Model/Exceptions/DuplicatedPatientInDbException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubaskyHospitalManager.Model.Exceptions
+{
+    public class DuplicatedPatientInDbException : Exception
+    {
+        public DuplicatedPatientInDbException()
+        {
+        }
+
+        public DuplicatedPatientInDbException(string message) : base(message)
+        {
+        }
+
+        public DuplicatedPatientInDbException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
