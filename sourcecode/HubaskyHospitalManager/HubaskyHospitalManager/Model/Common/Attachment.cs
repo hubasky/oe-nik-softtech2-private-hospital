3ee91abe8172d4f546d0a39c2ee2779ee7fce3f8
Hazai Péter <hazaip@niif.hu>
2016-05-03 23:41:00 +0200

    Database Model Complete - attachment fix

---
+++ sourcecode/HubaskyHospitalManager/HubaskyHospitalManager/Model/Common/Attachment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubaskyHospitalManager.Model.Common
+{
+    public class Attachment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string File { get; set; }
+
+        public Attachment()
+        {
+            File = "";
+        }
+
+        public Attachment(string path)
+        {
+            File = path;
+        }
+    }
+}
